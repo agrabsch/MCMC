dda07ed20589bfa99e47497844cddf0ea97c028c
grabsch.aurelien_140315 <aurelien.grabsch@gmail.ccom>
2018-01-04 15:52:52 +0100

    Update main

--- 1D/src/main.cpp
+++ 1D/src/main.cpp
@@ -2,8 +2,6 @@
 #include <fstream>
 #include <sstream>
 
-#include <complex>
-
 #include "mcmc.hpp"
 
 using namespace std;
@@ -18,51 +16,25 @@ const int Nsamples = 10000 ;
 int main()
 {
 
+  // generate N points
   MCMC sim(N) ;
 
   // forget init cdt
   sim.Iterate(2*M) ;
 
-  // values of interest
-  // largest eigenvalue
-  double LargEig(0) ;
-
-  // sum squares
-  double sx2(0) ;
-
   // output files
   ostringstream filename;
-  filename << "Sum_x2_N=" << N  << ".dat" ;
-  ofstream outX2(filename.str()) ;
+  filename << "pts_N=" << N  << ".dat" ;
+  ofstream out(filename.str()) ;
 
-  ostringstream filename2;
-  filename2 << "Largest_N=" << N  << ".dat" ;
-  ofstream outLarg(filename2.str()) ;
-  
+  vector<double> x = sim.get_points() ;
 
-
-  // get samples
-  for(int i=0; i<Nsamples; i++)
+  for(int j=0; j<N; j++)
     {
-      sim.Iterate(2*M) ;
-
-      vector<double> x = sim.get_points() ;
-
-      // sum x2 and largest
-      sx2 = 0 ;
-      LargEig = 0 ;
-      for(int j=0; j<N; j++)
-	{
-	  if(x[j] > LargEig) LargEig = x[j] ;
-	  
-	  sx2 += x[j]*x[j] ;
-	}
-      outX2 << sx2 << endl ;
-      outLarg << LargEig << endl ;
+      out << x[j] << endl ;
     }
 
-  outX2.close() ;
-  outLarg.close() ;
+  out.close() ;
 
   return 0 ;
 }
