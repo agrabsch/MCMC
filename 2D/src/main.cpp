55c23d379432b48bb17e2f432a4d6479ae25f2e9
grabsch.aurelien_140315 <aurelien.grabsch@gmail.ccom>
2018-01-04 15:57:38 +0100

    Final update

--- 2D/src/main.cpp
+++ 2D/src/main.cpp
@@ -8,53 +8,20 @@ using namespace std;
 
 const int N = 1000 ;
 
-const int M = 10*N ;
-
-const int Nsamples = 10000 ;
+const int M = 200*N ;
 
 
 int main()
 {
-
+  // realisation with N points
   MCMC sim(N) ;
 
-  sim.write_points("Init.dat") ;
-
   // forget init cdt
-  sim.Iterate(20*M) ;
+  sim.Iterate(M) ;
 
-  sim.write_points("Dist.dat") ;
+  // write result
+  sim.write_points("pts.dat") ;
 
-  /*
-  // largest eigenvalue
-  double LargEig(0) ;
-
- 
-  ostringstream filename2;
-  filename2 << "Largest_N=" << N  << ".dat" ;
-  ofstream outLarg(filename2.str()) ;
-  
-
-
-  // get samples
-  for(int i=0; i<Nsamples; i++)
-    {
-      sim.Iterate(2*M) ;
-
-      vector<complex<double> > z = sim.get_points() ;
-
-      // largest
-      LargEig = 0 ;
-      for(int j=0; j<N; j++)
-      	{
-      	  if(abs(z[j]) > LargEig) LargEig = abs(z[j]) ;
-      	}
-      outLarg << LargEig << endl ;
-    }
-
-  outLarg.close() ;
-
-  */
 
   return 0 ;
 }
